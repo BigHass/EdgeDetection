f30455d91db06c53a2bdf447a4d94b6625aca095
Husni Abdelsalam <94320854+BigHass@users.noreply.github.com>
2023-01-06 16:47:09 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,32 +15,34 @@ int main(int argc, char *argv[]) {
     h2o::Image::Ptr convolvedImage;
 
     int MASTER = handler.numOfProcs - 1;
-
+    // Declares variables for storing the number of rows and columns in the input image.
     int imgColumns{}, imgRows{};
     int desiredChannels = 1;
 
-    //For MPI_Gatherv.
+    // Declares vectors for use in the MPI_Gatherv function later.
     std::vector<int> displs(handler.numOfProcs), recivedCount(handler.numOfProcs);
 
-    //Read image by master proc.
+    // The master process reads the input image.
     if (handler.rank == MASTER) {
-
+        // Reads the input image from a file.
         image1D = h2o::Image::from_file("assets/9.jpg");
-
+        // Stores the number of columns and rows in the input image.
         imgColumns = image1D->columns();
         imgRows = image1D->rows();
-
+        // Prints the number of columns and rows in the input image.
         printf("Image Info: imgColumns:%d, imgRows:%d\n", imgColumns, imgRows);
     }
-
+    // Broadcasts the number of rows and columns in the input image to all processes.
     MPI_Bcast(&imgRows, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
     MPI_Bcast(&imgColumns, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+    // Initializes the convolved image with the same number of rows and columns as the input image.
     convolvedImage = h2o::Image::empty(imgRows, imgColumns);
-
+    
+    // If the current process is not the master, it initializes its image1D object as an empty image
     if (handler.rank != MASTER) {
         image1D = h2o::Image::empty(imgRows, imgColumns);
     }
-
+    // Broadcasts the data of the input image to all processes.
     MPI_Bcast(
             image1D->mut_data(),
             imgRows * imgColumns,
@@ -48,38 +50,43 @@ int main(int argc, char *argv[]) {
             MASTER,
             MPI_COMM_WORLD
     );
-
+    // Calculates the number of rows that each process will be responsible for processing.
     int offset = imgRows / handler.numOfProcs;
+    // Calculates the starting row for the current process.
     int rowStart = handler.rank * offset;
+    // Calculates the ending row for the current process.
     int rowEnd = rowStart + offset;
 
-    //Add extra rows to the master portion
+    //If the current process is the master process, it receives additional rows to process.
     if (handler.rank == MASTER) {
 
         rowEnd = rowEnd + imgRows % handler.numOfProcs;
     }
-
-    //For MPI_Gatherv.
+    // If the current process is the master process, it sets up the displs and recivedCount vectors.
+    //for use in the MPI_Gatherv function later.
     if (handler.rank == MASTER) {
 
         for (int i = 0; i < handler.numOfProcs; i++) {
             if (i == MASTER) {
+                // The master process receives the rows assigned to it plus any additional rows.
                 recivedCount[i] = (rowEnd - rowStart) * imgColumns;
             } else {
+                 // All other processes only receive the rows assigned to them.
                 recivedCount[i] = (rowEnd - (imgRows % handler.numOfProcs) - rowStart) * imgColumns;
             }
-
+            // Calculates the displacement for the current process in the final output image.
             displs[i] = (i == 0) ? 0 : displs[i - 1] + recivedCount[i - 1];
         }
     }
-
+    // Creates a ThreadArgs struct to store the arguments to be passed to the Sobel function.
     auto args = ThreadArgs{
-            .image = image1D,
-            .rowStart = rowStart,
-            .rowEnd = rowEnd,
+            .image = image1D,  // The input image.
+            .rowStart = rowStart, // The starting row for the current process.
+            .rowEnd = rowEnd, // The ending row for the current process.
     };
-
+    // Creates a std::promise object for storing the output image of the Sobel function.
     auto convolvedImagePartPromise = std::promise<h2o::Image::Ptr>{};
+    // Creates a std::future object for retrieving the output image from the std::promise object.
     auto convolvedImagePartFuture = convolvedImagePartPromise.get_future();
 
     std::jthread thread{
@@ -87,9 +94,11 @@ int main(int argc, char *argv[]) {
             args,
             std::move(convolvedImagePartPromise)
     };
+    // Waits for the Sobel function to finish executing.
     convolvedImagePartFuture.wait();
+    // Retrieves the output image from the std::future object.
     auto convolvedImagePart = convolvedImagePartFuture.get();
-
+    // Gathers the output images from all processes and combines them into the final output image.
     MPI_Gatherv(
             convolvedImagePart->span().data(),
             (rowEnd - rowStart) * imgColumns,
@@ -101,8 +110,9 @@ int main(int argc, char *argv[]) {
             MASTER,
             MPI_COMM_WORLD
     );
-
+    // If the current process is the master process, it writes the final output image to a file.
     if (handler.rank == MASTER) {
+        // Writes the output image to a file in the PNG format.
         stbi_write_png(
                 "assets/OUT.png",
                 imgColumns,
